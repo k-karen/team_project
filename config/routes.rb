df38d2fa2edcbf8b873a704bd415c4cadb65dc42
k-karen <k-karen@users.noreply.github.com>
2023-12-26 17:05:57 +0900

    fix by rubocop, add rubocop.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+    mount LetterOpenerWeb::Engine, at: '/letter_opener'
   end
 
-  get 'users/new'
-  get 'users/create'
   get 'login', to: 'sessions#new', as: :login
   post 'login', to: 'sessions#create'
   get 'sign_up', to: 'users#new', as: :sign_up
@@ -12,7 +10,7 @@ Rails.application.routes.draw do
   post 'sign_up', to: 'users#create'
   delete 'logout', to: 'sessions#destroy', as: :logout
 
-  root "home#index"
+  root 'home#index'
 
   get 'test', to: 'application#test'
 end
