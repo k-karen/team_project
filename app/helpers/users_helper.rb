5ec7074b5a87d6738d9cffa2671984b47dccfe6b
shunya9811 <11wash27@gmail.com>
2024-01-03 10:44:09 +0000

    Gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module UsersHelper
+  # 引数で与えられたユーザーのGravatar画像返す
+  def gravatar_for(user, options = { size: 80 })
+    size         = options[:size]
+    gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "gravatar")
+  end
 end
