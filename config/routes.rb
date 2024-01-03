3344080a4912ec5da49d38f3fd7605695b4a2702
shunya9811 <11wash27@gmail.com>
2024-01-03 08:00:04 +0000

    showアクションの作成

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,6 @@ Rails.application.routes.draw do
   get 'activate', to: 'users#activate'
   get 'sign_up', to: 'users#new', as: :sign_up
   post 'sign_up', to: 'users#create'
+
+  resources :users
 end
