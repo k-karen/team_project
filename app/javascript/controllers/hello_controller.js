8063d61749eeb0d7d63cc464df586d561fc62917
k-karen <k-karen@users.noreply.github.com>
2024-01-06 02:46:32 +0900

    fix for stimulas

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
+// コントローラークラスを定義する
 export default class extends Controller {
+  // ターゲット（操作対象のDOM）のプロパティを作成
+  static targets = [ "name" ]
+
+  // アクション（イベントに紐づく処理）を定義する
+  greet() {
+    // xxxxTargetでターゲットとなるDOMにアクセスできる
+    // ターゲット（今回であれば<input>）のvalueをログ吐き
+    console.log(this.nameTarget.value)
+  }
+
   connect() {
-    this.element.textContent = "Hello World!"
+    console.log("Hello World!")
   }
 }
