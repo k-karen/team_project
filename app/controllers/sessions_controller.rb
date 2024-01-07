1c1f9136169e4f82ff9b9c80419b5585f3fca080
shunya9811 <11wash27@gmail.com>
2024-01-07 15:47:54 +0000

    userコントローラーupdateアクション

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     if login(params[:email], params[:password])
       redirect_back_or_to(root_path, notice: "Login successful")
     else
-      flash.now[:alert] = "Login failed"
+      flash.now[:danger] = "Login failed"
       render(:new)
     end
   end
