7b990eae0b26ced3cd76373da0aab8d35de96e2a
k-karen <k-karen@users.noreply.github.com>
2024-01-11 02:42:55 +0900

    test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,11 +6,11 @@ class UserMailer < ApplicationMailer
   def activation_needed_email(user)
     @user = user
     @url  = "http://localhost:3000/activate?token=#{@user.activation_token}"
-    mail(to: user.email, subject: "Welcome to My Awesome Site")
+    mail(to: user.email, subject: "Welcome to My Awesome Site") if Rails.env.development?
   end
 
   def activation_success_email(user)
     @user = user
-    mail(to: user.email, subject: "Activation success")
+    mail(to: user.email, subject: "Activation success") if Rails.env.development?
   end
 end
