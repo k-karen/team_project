395fa19536d523b6f94aa617419a266a4d6dfa1a
KOJI.TSUKAMOTO <a2321kt@aiit.ac.jp>
2024-01-16 01:09:37 +0900

    add auto scroll messages. load page, send message ページをロードした時、メッセージを送った時にメッセージを最新の投稿まで自動的にスクロールする

--- app/javascript/auto_scroll_message.js
+++
@@ -1,7 +0,0 @@
-// メッセージを最新の投稿が表示されるように自動でスクロールしておく
-document.addEventListener("turbolinks:load", function() {
-    var messagesContainer = document.getElementById("messages");
-    if (messagesContainer) {
-      messagesContainer.scrollTop = messagesContainer.scrollHeight;
-    }
-});
