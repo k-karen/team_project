db4e700f087906d40654c5faca2ec31d2fa8d38f
k-karen <k-karen@users.noreply.github.com>
2024-01-18 01:44:55 +0900

    fix by rubocop

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 if Rails.env.production?
   creds = Aws::Credentials.new(
-  ENV['AWS_ACCESS_KEY_ID'],
-  ENV['AWS_SECRET_ACCESS_KEY']
+    ENV["AWS_ACCESS_KEY_ID"],
+    ENV["AWS_SECRET_ACCESS_KEY"],
   )
-  Aws::Rails.add_action_mailer_delivery_method(:aws_sdk, credentials: creds, region: 'ap-northeast-1')
+  Aws::Rails.add_action_mailer_delivery_method(:aws_sdk, credentials: creds, region: "ap-northeast-1")
 end
-
