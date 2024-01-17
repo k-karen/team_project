6e582116da9c9cfadbb63bd001bf378dee669fc2
k-karen <k-karen@users.noreply.github.com>
2024-01-17 23:55:23 +0900

    update script

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ git checkout main
 git pull
 ./update_config.sh
 
-export CURRENT_REVISION=$(git show --format='%h' --no-patch)
+export CURRENT_REVISION=$(git show --format='%h' --no-patch)----$(TZ=JST-9 date "+%Y/%m/%d %H:%M:%S")
 docker-compose --file docker-compose.production.yml down
 docker-compose --file docker-compose.production.yml up -d
