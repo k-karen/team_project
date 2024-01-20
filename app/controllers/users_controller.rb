eed7bbf8b29ddd8d3f2a68e393220ba4c532986b
k-karen <k-karen@users.noreply.github.com>
2024-01-20 22:58:10 +0900

    404用の処理を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,12 @@
 
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
-  before_action :check_access, only: [:show]
   def new
     @user = User.new
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user.friends.find(params[:id])
   end
 
   def create
