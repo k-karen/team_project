8e7c3754dd45fa2b3dcd3d4b99509213fd162391
shunya9811 <11wash27@gmail.com>
2024-01-22 11:48:33 +0000

    ageとbiograpyとiconを編集できるページを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit_settings, :update_settings, :edit_profile, :update_profile]
   def new
     @user = User.new
   end
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new(user_settings_params)
     if @user.save
       redirect_to(login_path, notice: "User was successfully created. Please check your email to activate.")
     else
@@ -34,25 +34,44 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    # TODO: biograpy等の編集と、パスワード等の変更を分ける
+  def edit_settings
     @user = current_user
   end
 
-  def update
+  def update_settings
     @user = current_user
-    if @user.update(user_params)
+    if @user.update(user_settings_params)
+      flash[:notice] = "Settings updated"
+      redirect_to(@user)
+    else
+      flash[:alert] = "Failed to update settings"
+      render(:edit_settings)
+    end
+  end
+
+  def edit_profile
+    @user = current_user
+  end
+
+  def update_profile
+    @user = current_user
+    if @user.update(user_profile_params)
       flash[:notice] = "Profile updated"
       redirect_to(@user)
     else
-      render(:edit)
+      flash[:alert] = "Failed to update profile"
+      render(:edit_profile)
     end
   end
 
   private
 
-  def user_params
+  def user_settings_params
     params.require(:user).permit(:email, :password, :username)
+  end
+
+  def user_profile_params
+    params.require(:user).permit(:biography, :age)
   end
 
   # beforeフィルタ
