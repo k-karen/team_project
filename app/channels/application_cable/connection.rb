a4ebc2d34e48aa88f74a1cbf43f8b97f94c455f3
k-karen <k-karen@users.noreply.github.com>
2024-01-23 04:06:57 +0900

    remove unused files

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,10 +2,5 @@
 
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :current_user
-
-    def connect
-      self.current_user = User.find_by(id: request.session.fetch("user_id", nil)) || reject_unauthorized_connection
-    end
   end
 end
